6e9adf82c8e777c5acd83be72958283db9451277
Devanir Cordeiro Soares Junior <devanirsoaresjr@gmail.com>
2018-12-10 10:37:56 +0000

    Update app/src/main/java/com/example/deva_/week5devanirgithubapimvp/base/BaseActivity.kt

---
+++ app/src/main/java/com/example/deva_/week5devanirgithubapimvp/base/BaseActivity.kt
@@ -0,0 +1,22 @@
+package com.example.deva_.week5devanirgithubapimvp.base
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+abstract class BaseActivity<T:BasePresenter> : AppCompatActivity(), BaseView{
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    init()
+    }
+
+    override fun showMessage(message: String) {
+
+    }
+
+    override fun showError(error: String) {
+
+    }
+    abstract fun init()
+    abstract fun getViewPresenter(): T
+}
